5026fe8f65fa94a5531b97942161c4c668ac9d57
Dana Walsh <dana.e.walsh@gmail.com>
2021-05-18 16:33:02 -0700

    Navbar is rendering correctly. React router has not been properly setup

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -15,40 +15,36 @@ import ContactMe from './ContactMe.jsx';
 class NavigationBar extends React.Component {
   render() {
     return (
-      <div>
-        <div className="row">
-          <div className="col-md-12">
-            <Router>
-              <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                <Navbar.Brand href="#home">Dana Walsh</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="mr-auto">
-                    <Nav.Link href="/">About</Nav.Link>
-                    <Nav.Link href="/projects">Projects</Nav.Link>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-              </Navbar>
-              <br />
-              <Switch>
-                <Route exact path="/">
-                  {/* <Home /> */}
-                </Route>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/projects">
-                  <Projects />
-                </Route>
-                <Route path="/contact-me">
-                  <ContactMe />
-                </Route>
-              </Switch>
-            </Router>
-          </div>
-        </div>
-      </div>
+      <>
+        <Router>
+          <Navbar bg="dark" variant="dark" expand="lg" sticky="top" style={{ marginBottom: '-24px' }}>
+            <Navbar.Brand href="#home">Dana Walsh</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">About</Nav.Link>
+                <Nav.Link href="/projects">Projects</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <br />
+          <Switch>
+            <Route exact path="/">
+              {/* <Home /> */}
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/contact-me">
+              <ContactMe />
+            </Route>
+          </Switch>
+        </Router>
+      </>
     );
   }
 }
