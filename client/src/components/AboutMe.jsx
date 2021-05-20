34bd01975b98ea54a512e3115435cc39f657bb33
Dana Walsh <dana.e.walsh@gmail.com>
2021-05-20 11:02:36 -0700

    Basics for AboutMe section completed. Text and photo in position. Not yet responsive

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -4,8 +4,45 @@ import SectionDiv from './sharedStyles/sectionDiv.js';
 
 const AboutMe = () => (
   <SectionDiv id="about">
-    Hi, I am Dana
+    <Container>
+      <FlexComponent style={{ flexDirection: 'column',  }}>
+        <InnerDiv>
+          Hello world!
+        </InnerDiv>
+        <InnerDiv style={{ padding: '5px 50px' }}>
+          My name is Dana Walsh, and I am a Full Stack Software Engineer. I am passionate about technology that makes a difference and tangibly improves the world. I am currently working on passion projects while on the look out for new employment opportunities. I have a diverse background ranging from tourism to tech sales to healthcare. I bring a unique and well rounded perspective to any team, and pride myself on being the ultimate team player.
+        </InnerDiv>
+        <InnerDiv style={{ padding: '20px 50px' }}>
+          When I'm not coding you'll find me in my garden or out on a hike, and I'm always accompanied by my two rescue dogs. Please feel free to reach out with any questions or if you just want to chat!
+        </InnerDiv>
+      </FlexComponent>
+      <FlexComponent>
+        <Image src="swissPhoto.png" alt="Photo in the Swiss Alps"/>
+      </FlexComponent>
+    </Container>
   </SectionDiv>
 );
 
 export default AboutMe;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const FlexComponent = styled.div`
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InnerDiv = styled.div`
+  padding: 20px;
+`;
+
+const Image = styled.img`
+  height: 65%;
+  border-radius: 5px;
+`;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -21,9 +21,9 @@ const NavigationBar = () => (
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/">Resume</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link target='_blank' href="https://drive.google.com/file/d/1LNUt5R5A7lWYyDZy6csBfWb6cbTSoelG/view?usp=sharing">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
