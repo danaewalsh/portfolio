80268c68cf2801c0901b9ac4a56166a2b632d108
Dana Walsh <dana.e.walsh@gmail.com>
2021-05-24 17:59:47 -0700

    building out the projects array

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -3,7 +3,7 @@ export const projectData = [
     title: 'AdiDOS',
     tech_stack: 'React, Express, CircleCI, Enzyme, Jest, EC2, Styled-Components',
     overview:
-      'Managed a team of engineers tasked with a complete redesign for an outdated ecommerce website. I modernized the related products section with a product carousel and a method for saving products for later.',
+      'Managed a team of engineers tasked with a complete redesign for an outdated e-commerce website. I modernized the related products section with a product carousel and a method for saving products for later.',
     features: 'Responsive layout, accessibility',
     image: 'relatedProducts_FEC.png',
     github_link: 'https://github.com/Observant-Ostriches/fec-projectCatwalk',
@@ -13,6 +13,15 @@ export const projectData = [
     tech_stack: 'MySQL, Express, EC2, S3, Docker, Loader.io, K6, Nginx',
     overview:
       'I built out and optimized the back end API for the Products data of an e-commerce website. This included cleaning and loading 50 millions records into a MySQL database, optimizing query speeds to <2ms, load-testing via K6 and Loader.io, containerizing services via Docker.io, and implementing a Nginx load balancer.',
+    features: '',
+    image: 'Loader.io.png',
+    github_link: 'https://github.com/Mighty-Mesquite/products_api',
+  },
+  {
+    title: 'Sage Advice',
+    tech_stack: 'MongoDB, Mongoose, Express, Styled-Components, Material-UI',
+    overview:
+      'I was challenged to complete a full stack project in 2 days, so I built this garden-assistant application. Sage Advice tracks the plants in a user\'s garden, and uses data from an external API to present plant specific information',
     features: '',
     image: 'Loader.io.png',
     github_link: 'https://github.com/Mighty-Mesquite/products_api',
